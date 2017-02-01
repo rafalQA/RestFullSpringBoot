96b4c0843f45e2a91fa940da1adcabcb95a1da93
Rafał <rpiotrowicz@pgs-soft.com>
2017-02-01 16:20:26 +0100

    Fix: Add api operation annotation to property controller

--- src/main/java/com/possessor/controller/PropertyController.java
+++ src/main/java/com/possessor/controller/PropertyController.java
@@ -4,6 +4,7 @@ import com.possessor.model.ForeignCurrency;
 import com.possessor.model.Property;
 import com.possessor.service.PropertyService;
 import com.possessor.service.UserService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -23,30 +24,35 @@ public class PropertyController {
     @Autowired
     private PropertyService propertyService;
 
+    @ApiOperation(value = "addPropertyForUser", nickname = "addPropertyForUser")
     @RequestMapping(method = RequestMethod.PUT, value = "/user/{id}/property")
     @ResponseStatus(HttpStatus.CREATED)
     public Long addPropertyForUser(@PathVariable Long id, @RequestBody Property property) {
        return propertyService.addPropertyForUser(id, property);
     }
 
+    @ApiOperation(value = "deletePropertyWhichHasUser", nickname = "deletePropertyWhichHasUser")
     @RequestMapping(method = RequestMethod.DELETE, value = "/user/{id}/property")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deletePropertyWhichHasUser(@PathVariable Long id) {
         propertyService.deletePropertyWithUser(id);
     }
 
+    @ApiOperation(value = "getAllPropertiesForUser", nickname = "getAllPropertiesForUser")
     @RequestMapping(method = RequestMethod.GET, value = "/user/{id}/properties")
     @ResponseStatus(HttpStatus.OK)
     public List<Property> getAllPropertiesForUser(@PathVariable Long id) {
         return propertyService.getAllPropertiesForUser(id);
     }
 
+    @ApiOperation(value = "getAllProperties", nickname = "getAllProperties")
     @RequestMapping(method = RequestMethod.GET, value = "/properties")
     @ResponseStatus(HttpStatus.OK)
     public List<Property> getAllProperties() {
         return propertyService.getAllProperties();
     }
 
+    @ApiOperation(value = "getValueByForeignCurrency", nickname = "getValueByForeignCurrency")
     @RequestMapping(method = RequestMethod.GET, value = "/{id}")
     @ResponseStatus(HttpStatus.OK)
     public BigDecimal getValueByForeignCurrency(@PathVariable Long id,
